7c2f06f6e0b8514f0fef3b29bbe2ae1a0fc5bc85
Cody Wilson <cody.wilson9495@gmail.com>
2023-08-01 19:52:23 -0700

    feat: Add AcademicFitAthleteImage component & test

---
+++ src/components/AcademicFitAthleteImage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="overflow-hidden rounded-full w-28 h-28 athlete-image">
+    <img class="rounded-full athlete-image__image"
+      v-if="imageUrl"
+      :src="imageUrl"
+      :alt="athleteName">
+    <div v-else
+      :class="computedAthleteImgFallbackClasses">
+      {{ athleteInitials }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, computed } from 'vue';
+
+const props = defineProps({
+  imageUrl: {
+    type: String,
+    required: false,
+  },
+  athleteName: {
+    type: String,
+    required: true,
+  },
+});
+
+// This is a map between regex patterns and associated colors.
+// The regex is used to match the first letter of the athlete's last name.
+// We're using the full TailwindCSS arbitrary values for the colors explicitly so they're picked up by PurgeCSS.
+const regexTailwindBgColorMap = {
+  '^[A-E]': 'bg-[#f1603c]',
+  '^[F-I]': 'bg-[#6082fa]',
+  '^[J-M]': 'bg-[#827cb8]',
+  '^[N-Q]': 'bg-[#0097a4]',
+  '^[R-U]': 'bg-[#ffe066]',
+  '^[V-Z]': 'bg-[#ffa94d]',
+}
+
+// This computed property returns the first matching regex pattern from the map above for the given athlete's last name.
+const computedAthleteImgFallbackClasses = computed(() => {
+  return Object.keys(regexTailwindBgColorMap).map(key => {
+    const regex = new RegExp(key);
+    if (regex.test(props.athleteName.split(' ')[1])) {
+      return `athlete-image__fallback flex items-center justify-center h-full w-full font-bold text-xl ${regexTailwindBgColorMap[key]}`;
+    }
+  });
+});
+
+const athleteInitials = computed(() => {
+  const name = props.athleteName;
+  const initials = name.match(/\b\w/g) || [];
+  return ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+});
+</script>
---
+++ tests/unit/AcademicFitAthleteImage.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils';
+
+import AcademicFitAthleteImage from '../../src/components/AcademicFitAthleteImage.vue';
+
+describe('AcademicFitAthleteImage', () => {
+    it('should display an image given an imageUrl prop', () => {
+        const wrapper = shallowMount(AcademicFitAthleteImage, {
+            propsData: {
+                athleteName: 'Test Athlete',
+                imageUrl: 'https://via.placeholder.com/150',
+            },
+        });
+
+        expect(wrapper.find('img').attributes('src')).toBe('https://via.placeholder.com/150');
+    });
+
+    it('should display an image fallback when an imageUrl prop is not present', () => {
+        const wrapper = shallowMount(AcademicFitAthleteImage, {
+            propsData: {
+                athleteName: 'Test Athlete',
+            },
+        });
+
+        // Ensure an image is not present
+        expect(wrapper.find('img').exists()).toBe(false);
+        // Ensure the initials are displayed
+        expect(wrapper.text()).toContain('TA');
+        // Ensure the correct background color is displayed given the first letter of the athlete's last name
+        expect(wrapper.find('.athlete-image__fallback').classes()).toContain('bg-[#f1603c]');
+    });
+});
