f7caf01344f01f1cf877b684c6591f7ff33be6ea
Cody Wilson <cody.wilson9495@gmail.com>
2023-08-02 21:05:45 -0700

    fix: Harden tests for AcademicFitAthleteImage

--- tests/unit/AcademicFitAthleteImage.spec.js
+++ tests/unit/AcademicFitAthleteImage.spec.js
@@ -2,6 +2,15 @@ import { shallowMount } from '@vue/test-utils';
 
 import AcademicFitAthleteImage from '../../src/components/AcademicFitAthleteImage.vue';
 
+const athleteNameColorMap = {
+  'Amanda Athlete': 'bg-[#f1603c]',
+  'Freethrow Francis': 'bg-[#6082fa]',
+  'Layup Larryson': 'bg-[#827cb8]',
+  'Overtime Ollie': 'bg-[#0097a4]',
+  'Slamdunk Sally': 'bg-[#ffe066]',
+  'Yellowcard Yolanda': 'bg-[#ffa94d]',
+}
+
 describe('AcademicFitAthleteImage', () => {
   it('should display an image given an imageUrl prop', () => {
     const wrapper = shallowMount(AcademicFitAthleteImage, {
@@ -15,17 +24,32 @@ describe('AcademicFitAthleteImage', () => {
   });
 
   it('should display an image fallback when an imageUrl prop is not present', () => {
-    const wrapper = shallowMount(AcademicFitAthleteImage, {
-      propsData: {
-        athleteName: 'Test Athlete',
-      },
-    });
+    for (const [athleteName, bgColor] of Object.entries(athleteNameColorMap)) {
+      const wrapper = shallowMount(AcademicFitAthleteImage, {
+        propsData: {
+          athleteName,
+        },
+      });
 
-    // Ensure an image is not present
-    expect(wrapper.find('img').exists()).toBe(false);
-    // Ensure the initials are displayed
-    expect(wrapper.text()).toContain('TA');
-    // Ensure the correct background color is displayed given the first letter of the athlete's last name
-    expect(wrapper.find('.athlete-image__fallback').classes()).toContain('bg-[#f1603c]');
+      // Ensure an image is not present
+      expect(wrapper.find('img').exists()).toBe(false);
+      // Ensure the initials are displayed
+      expect(wrapper.text()).toContain(athleteName.split(' ').map((name) => name[0]).join(''));
+      // Ensure the correct background color is displayed given the first letter of the athlete's last name
+      expect(wrapper.find('.athlete-image__fallback').classes()).toContain(bgColor);
+    }
+
+    // const wrapper = shallowMount(AcademicFitAthleteImage, {
+    //   propsData: {
+    //     athleteName: 'Test Athlete',
+    //   },
+    // });
+
+    // // Ensure an image is not present
+    // expect(wrapper.find('img').exists()).toBe(false);
+    // // Ensure the initials are displayed
+    // expect(wrapper.text()).toContain('TA');
+    // // Ensure the correct background color is displayed given the first letter of the athlete's last name
+    // expect(wrapper.find('.athlete-image__fallback').classes()).toContain('bg-[#f1603c]');
   });
 });
